e3cf42d68c2c8cee7ff0ba4e993ece06786b5fbd
sai divya <Sai.Divya@iiitb.org>
2018-05-09 01:57:18 +0530

    mini project

--- src/test/java/com/example/Bank/AppTest.java
+++ src/test/java/com/example/Bank/AppTest.java
@@ -2,9 +2,17 @@ package com.example.Bank;
 
 import java.util.*;
 import java.lang.*;
+import java.io.*;
+import org.junit.*;
+import org.junit.Assert;
+
 
 
 public class AppTest{
+	@Test
+	
+	public void getDate(){
+}
 
 
 
